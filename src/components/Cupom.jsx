afc1dd9cffd13eaa1f50d6d654435c6a239531f6
morphis-code <morphis.code@gmail.com>
2025-03-22 18:03:02 -0300

    Changing the icon and adjusting the name, the description of the browser tab and making the coupons on the '/' page clickable

--- src/components/Cupom.jsx
+++ src/components/Cupom.jsx
@@ -1,21 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Cupom({porcent,name, description}){
     return(
-        <div className="flex flex-col bg-black p-5 rounded-xl gap-3 mb-4">
-            <div className="flex items-center justify-between">
-                <h1 className="text-white font-medium text-xl">{`${porcent}%  ${name}`}</h1>
-                <div className="relative h-7 w-7">
-                    <Image
-                    src="/porcent.svg"
-                    alt="porcentagem"
-                    layout="fill"
-                    />
+        <Link href="/ops">
+            <div className="flex flex-col bg-black p-5 rounded-xl gap-3 mb-4 cursor-pointer">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-white font-medium text-xl">{`${porcent}%  ${name}`}</h1>
+                    <div className="relative h-7 w-7">
+                        <Image
+                        src="/porcent.svg"
+                        alt="porcentagem"
+                        layout="fill"
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <p className="text-gray-500 rounded-lg line-clamp-2 peer">{description}</p>
-            </div>
-        </div>
+                <div>
+                    <p className="text-gray-500 rounded-lg line-clamp-2 peer">{description}</p>
+                </div>
+            </div>       
+        </Link>
+       
     )
 }
