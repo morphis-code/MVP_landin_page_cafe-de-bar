e3af7c1ebb79ac86039e8d5640920bff88c65c03
Morphis <morphis.code@gmail.com>
2025-03-24 22:04:15 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
           <Link href="/ops" className=""><button className="bg-black cursor-pointer text-white p-3 text-2xl font-medium rounded-lg w-64">Retire cupons</button></Link>
         </div>
         <div className="px-2 mt-2 pb-2 w-full max-w-[500px]">
-          <Cupom porcent="10" name="Chopp Burgman" description="esse é um cupom comum para que os consumidores possam estar aproveitando da melhor forma as igrarias dos estabelecimentos" />
+          <Cupom porcent="10" name="Chopp To a Tôa" description="esse é um cupom comum para que os consumidores possam estar aproveitando da melhor forma as igrarias dos estabelecimentos" />
           <Cupom porcent="5" name="To a Tôa" description="aproveite da melhor forma as iguarias do estabelecimento" />
         </div>
       
